0dabef8b1ca49d3418b0e4e43e325f9bb947a243
Aneeket <aneeketmangal@gmail.com>
2021-05-29 15:03:25 +0530

    make it better

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import requests, json
 import jsonify
 import time
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://anee.pythonanywhere.com/"
 
 response = requests.get(BASE + "login/"+"tempUser")
-print(response.json())
+print(response)
+print(response.content)
 
 
 dd = {}
@@ -14,13 +15,13 @@ dd = {}
 while(True):
     s = input()
     t = input()
-    BASE = "http://127.0.0.1:5000/send"
+    BASE = "http://anee.pythonanywhere.co/send"
     d ={"name":"tempUser", "user":s, "message":t}
     response = requests.get(BASE, params=d)
     # print(response.url)
     print(response.json())
     d = {"name":"tempUser"}
-    BASE = "http://127.0.0.1:5000/recieve"
+    BASE = "http://anee.pythonanywhere.com/recieve"
     response = requests.get(BASE, params=d)
     # print(response.url)
     print(response.json())
